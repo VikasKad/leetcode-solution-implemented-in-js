a07890ee75403ac1723d727237fe42c5ccdf8aad
Vikas <vikasrkad@gmail.com>
2022-09-13 23:13:59 +0530

    pushed final code

--- Pattern Programs/patterns.js
+++ Pattern Programs/patterns.js
@@ -176,3 +176,145 @@ console.log(printRightDecreasingTriangle(count));
 }
 console.log(`-------6. Hill Pattern  ----\n`)
 console.log(printHillPattern(count));
+
+
+/**
+ * 7. Reverse Hill Pattern 
+        *********
+         *******
+          *****         
+           ***
+            *
+ */
+const printReverseHillPattern = (n) => {
+    let str = "";
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= i; j++) {
+            str += "  ";
+        }
+        for(let j=i;j<n;j++){
+            str+='* '
+        }
+
+        for(let j=i;j<=n;j++){
+            str+='* '
+        }
+        str += '\n';
+    }
+    return (str);
+}
+console.log(`-------7. Reverse Hill Pattern  ----\n`)
+console.log(printReverseHillPattern(count));
+
+
+/**
+ * 8. Diamond  Pattern 
+            *
+           ***
+          *****
+         *******
+        *********
+        *********
+         *******
+          *****         
+           ***
+            *
+ */
+const printDiamondPattern = (n) => {
+    let str = "";
+    for(let i=1;i<n;i++){
+        for(let j=i;j<=n;j++){
+            str +="  ";
+        }
+        for(let j=1;j<i;j++){
+            str +="* ";
+        }
+
+        for(let j=1;j<=i;j++){
+            str +="* ";
+        }
+        str+='\n';
+    }
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= i; j++) {
+            str += "  ";
+        }
+        for(let j=i;j<n;j++){
+            str+='* '
+        }
+
+        for(let j=i;j<=n;j++){
+            str+='* '
+        }
+        str += '\n';
+    }
+    return (str);
+}
+console.log(`------- 8. Diamond  Pattern ----\n`)
+console.log(printDiamondPattern(count));
+
+
+
+/**
+ * 9.   Hollow Triangle Pattern 
+*
+**
+* *
+*  *
+*   *
+******
+   
+ */
+ const printHollow = (n) => {
+    let str = "";
+    for(let i=1;i<=n;i++){
+        for(let j=1;j<=i;j++){
+            if(i===n){
+                str+="* ";
+            }else if(j===1 || j===i ){
+                str+="* "
+            }else{
+                str+="  ";
+            }
+        }
+        str+='\n';
+    }
+    return (str);
+}
+console.log(`------- 9.   Hollow Triangle Pattern ----\n`)
+console.log(printHollow(count));
+        
+
+/**
+ * 10.  Hollow Pyramid Star Pattern 
+    *
+   * *
+  *   *
+ *     *
+*********
+ */
+ const printHollowPyramid = (n) => {
+    let str = "";
+    for(let i=1;i<=n;i++){
+        for(let j=i;j<=n;j++){
+            str+=" ";
+        }
+        for (let k = 0; k < 2 * i - 1; k++) {
+            if(i === 1 || i === n) {
+                str += "*";
+            }
+            else {
+              if(k === 0 || k === 2 * i - 2) {
+                str += "*";
+              }
+              else {
+                str += " ";
+              }
+            }
+          }
+          str += "\n";
+        }
+    return (str);
+}
+console.log(`------- 10.  Hollow Pyramid Star Pattern ----\n`)
+console.log(printHollowPyramid(count));
