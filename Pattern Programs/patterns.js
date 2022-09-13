b7c0fc912e54c49faa7539238ee922522f58c372
Vikas <vikasrkad@gmail.com>
2022-09-13 15:06:27 +0530

    updated the pattern programs

--- Pattern Programs/patterns.js
+++ Pattern Programs/patterns.js
@@ -1,3 +1,5 @@
+let count=5;
+
 /*
     1.   Square Star Pattern program
     
@@ -7,18 +9,18 @@
     *****
     *****
 */
-let n = 5;
 const printSquareStar = (n) => {
     let str = "";
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n; j++) {
-            str += "*";
+            str += "* ";
         }
         str += '\n';
     }
-    console.log(str);
+    return (str);
 }
-// printSquareStar(5);
+console.log(`--------1.   Square Star Pattern program----\n`)
+console.log(printSquareStar(count));
 
 /**
  * 2. Hollow Square Pattern
@@ -33,15 +35,114 @@ const printSquarePattern = (n) => {
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n; j++) {
             if (i === 0 || i === n - 1) {
-                str += "*";
+                str += "* ";
             } else if (j === 0 || j === n - 1) {
-                str += "*";
+                str += "* ";
             } else {
-                str += " ";
+                str += "  ";
             }
         }
         str += '\n';
     }
-    console.log(str);
+    return (str);
 }
-printSquarePattern(5);
+console.log(`--------2. Hollow Square Pattern----\n`)
+console.log(printSquarePattern(count));
+
+/**
+ * 3. Increasing Pattern 
+    *
+    *  *
+    *  *  *
+    *  *  *  *
+    *  *  *  *  *
+
+ */
+ const increasingTriangle = (n) => {
+    let str = "";
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= i; j++) {
+            str += "* ";
+        }
+        str += '\n';
+    }
+    return(str);
+}
+console.log(`--------3. Increasing Pattern ----\n`)
+
+console.log(increasingTriangle(count));
+
+/**
+ * 4. Decreasing Pattern
+    
+    *  *  *  *  *
+    *  *  *  *
+    *  *  *
+    *  *  
+    *  
+    
+
+ */
+ const decreasingTriangle = (n) => {
+    let str = "";
+    for (let i = 1; i <= n; i++) {
+        for(let j=i;j<=n;j++){
+            str+='* '
+        }
+        str += '\n';
+    }
+    return(str);
+}
+console.log(`--------4. Decreasing Pattern----\n`)
+
+console.log(decreasingTriangle(count));
+
+/**
+ * 5. Right side increasing Triangle Pattern 
+    *
+   **
+  ***
+ ****
+*****
+
+ */
+const printRightTriangle = (n) => {
+    let str = "";
+    for (let i = 1; i <= n; i++) {
+        for (let j = i; j <= n; j++) {
+            str += "  ";
+        }
+        for(let j=1;j<=i;j++){
+            str+='* '
+        }
+        str += '\n';
+    }
+    return (str);
+}
+console.log(`-------6. Right side decreasing Triangle Pattern ----\n`)
+console.log(printRightTriangle(count));
+
+/**
+ * 5. Right side decreasing Triangle Pattern 
+    
+    * * * * *
+      * * * *
+        * * *
+          * *
+            *
+ */
+const printRightDecreasingTriangle = (n) => {
+    let str = "";
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= i; j++) {
+            str += "  ";
+        }
+        for(let j=i;j<=n;j++){
+            str+='* '
+        }
+        str += '\n';
+    }
+    return (str);
+}
+console.log(`-------5. Right side decreasing Triangle Pattern ----\n`)
+console.log(printRightDecreasingTriangle(count));
